5e03996d01da36e84f8a4b4106e8edbe96ad2813
Gabriel Marchidan <gabriel.marchidan@feelit.iasi.net>
2016-11-01 21:28:47 +0200

    Added initial Template

---
+++ POC/GovItHub.EmailClassifier/GovItHub.EmailClassifier/Migrations/EmailClassifierContextModelSnapshot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using GovItHub.EmailClassifier.Models;
+
+namespace GovItHub.EmailClassifier.Migrations
+{
+    [DbContext(typeof(EmailClassifierContext))]
+    partial class EmailClassifierContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+
+            modelBuilder.Entity("GovItHub.EmailClassifier.Models.EmailClasses", b =>
+                {
+                    b.Property<int>("EmailClassId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("EmailClassId");
+
+                    b.ToTable("EmailClasses");
+                });
+        }
+    }
+}
---
+++ POC/GovItHub.EmailClassifier/GovItHub.EmailClassifier/Models/EmailClassifierContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GovItHub.EmailClassifier.Models
+{
+    public class EmailClassifierContext : DbContext
+    {
+        public EmailClassifierContext(DbContextOptions<EmailClassifierContext> options)
+            : base(options)
+        { }
+
+        public DbSet<EmailClasses> EmailClasses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<EmailClasses>().HasKey(m => m.EmailClassId);
+            
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
