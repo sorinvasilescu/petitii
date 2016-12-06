4d24a2ccebcdbf78abb06f159e33b29496e4c284
Sorin Vasilescu <sorin.vasilescu@gmail.com>
2016-12-06 22:47:25 +0200

    error handling and config parsing

---
+++ src/main/java/ro/petitii/config/Config.java
@@ -0,0 +1,10 @@
+package ro.petitii.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties
+public class Config {
+
+}
--- src/main/java/ro/petitii/controller/MainController.java
+++ src/main/java/ro/petitii/controller/MainController.java
@@ -1,10 +1,16 @@
 package ro.petitii.controller;
 
+import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MainController {
+public class MainController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String error() {
+        return "error";
+    }
 
     @RequestMapping("/")
     public String index() {
@@ -14,5 +20,10 @@ public class MainController {
     @RequestMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
     }
 }
