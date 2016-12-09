17536dec94f980a73f24a96754bf70e236b90dc6
Sorin Vasilescu <sorin.vasilescu@gmail.com>
2016-12-09 13:19:04 +0200

    unified email and spam templates and deleted spam template

--- src/main/java/ro/petitii/controller/EmailController.java
+++ src/main/java/ro/petitii/controller/EmailController.java
@@ -1,18 +1,33 @@
 package ro.petitii.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ro.petitii.config.ImapConfig;
 
 @Controller
 public class EmailController {
+    
+    @Autowired
+    ImapConfig config;
 
     @RequestMapping("/inbox")
-    public String inbox() {
-        return "email_page";
+    public ModelAndView inbox() {
+        ModelAndView modelAndView = new ModelAndView("email_page");
+        modelAndView.addObject("title","Email-uri primite");
+        modelAndView.addObject("email", config.getUsername());
+        modelAndView.addObject("restUrl","/rest/emails");
+        return modelAndView;
     }
 
     @RequestMapping("/spam")
-    public String spam() {
-        return "spam_page";
+    public ModelAndView spam() {
+        ModelAndView modelAndView = new ModelAndView("email_page");
+        modelAndView.addObject("title","Spam");
+        modelAndView.addObject("email", config.getUsername());
+        modelAndView.addObject("restUrl","/rest/spam");
+        return modelAndView;
     }
 }
