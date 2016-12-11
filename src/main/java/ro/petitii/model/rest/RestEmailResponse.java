9b64d1aa2d34d66e5c6e9f4df6844bb9d9a9276c
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-12-11 20:54:49 +0200

    PET-19 removed properties that are already implemented in datatablesoutput

--- src/main/java/ro/petitii/model/rest/RestEmailResponse.java
+++ src/main/java/ro/petitii/model/rest/RestEmailResponse.java
@@ -1,44 +1,7 @@
 package ro.petitii.model.rest;
 
-import java.util.List;
+import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
 
-public class RestEmailResponse {
-    int draw;
-    long recordsTotal;
-    long recordsFiltered;
-    List<RestEmailResponseElement> data;
+public class RestEmailResponse extends DataTablesOutput<RestEmailResponseElement>{
 
-    public RestEmailResponse() {}
-
-    public int getDraw() {
-        return draw;
-    }
-
-    public void setDraw(int draw) {
-        this.draw = draw;
-    }
-
-    public long getRecordsTotal() {
-        return recordsTotal;
-    }
-
-    public void setRecordsTotal(long recordsTotal) {
-        this.recordsTotal = recordsTotal;
-    }
-
-    public long getRecordsFiltered() {
-        return recordsFiltered;
-    }
-
-    public void setRecordsFiltered(long recordsFiltered) {
-        this.recordsFiltered = recordsFiltered;
-    }
-
-    public List<RestEmailResponseElement> getData() {
-        return data;
-    }
-
-    public void setData(List<RestEmailResponseElement> data) {
-        this.data = data;
-    }
 }
