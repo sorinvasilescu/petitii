d6729397034a1f20bc7cef428ca80e392b61a658
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-12-11 20:55:55 +0200

    PET-19 added findall method for datatablesinput, used to display the user list

--- src/main/java/ro/petitii/service/UserService.java
+++ src/main/java/ro/petitii/service/UserService.java
@@ -1,8 +1,13 @@
 package ro.petitii.service;
 
+import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
+import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
 import ro.petitii.model.User;
+
 import java.util.List;
 
 public interface UserService {
     List<User> findUserByEmail(String email);
+
+    DataTablesOutput<User> findAll(DataTablesInput input);
 }
