83e450c771e381fdaae116f61d57b6afa503906d
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-12-11 20:55:24 +0200

    PET-19 extended userrepository from datatablesrepository

--- src/main/java/ro/petitii/repository/UserRepository.java
+++ src/main/java/ro/petitii/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package ro.petitii.repository;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.datatables.repository.DataTablesRepository;
 import org.springframework.stereotype.Repository;
 import ro.petitii.model.User;
 
 import java.util.List;
 
 @Repository
-public interface UserRepository extends PagingAndSortingRepository<User,Long> {
+public interface UserRepository extends DataTablesRepository<User, Long> {
     List<User> findByEmail(String email);
 }
