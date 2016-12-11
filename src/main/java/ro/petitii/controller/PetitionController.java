dfb11e59d81f3fd3b694b85bc47ff8e07a4ccc31
Sorin Vasilescu <sorin.vasilescu@gmail.com>
2016-12-11 10:12:57 +0200

    joined controllers

--- src/main/java/ro/petitii/controller/PetitionController.java
+++ src/main/java/ro/petitii/controller/PetitionController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class PetitionsController {
+public class PetitionController {
+
+    @RequestMapping("/addPetition")
+    public String addPetition() {
+        return "add_petition";
+    }
 
     @RequestMapping("/petitii")
-    public String petitii() {
+    public String listPetitions() {
         return "petitii_page";
     }
 }
