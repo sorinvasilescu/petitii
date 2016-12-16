410ed5261147db536225e2fc65ea2f4e35c629b6
Sorin Vasilescu <sorin.vasilescu@gmail.com>
2016-12-16 02:32:09 +0200

    fixed broken stuff

--- src/main/java/ro/petitii/config/SecurityConfig.java
+++ src/main/java/ro/petitii/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/css/**","/js/**","/images/**","/webjars/**","/vendors/**","/rest/**")
+                .antMatchers("/css/**","/js/**","/img/**","/webjars/**","/vendors/**","/rest/**")
                     .permitAll()
                 .anyRequest()
                     .authenticated()
