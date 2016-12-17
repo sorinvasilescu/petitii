2f1aee585112e86ecb2935d98c7c6066916b73d1
gsergiu <sergiu.gordea@ait.ac.at>
2016-12-17 12:22:36 +0100

    #45 scripts for adding dummy contact data into database, added phone to database, added code for listing contacts and performed some refactorings (improving naming)
    Change-Id: I9cea12eb7e4cb0cede1004eac339f3c616b63574

---
+++ src/main/java/ro/petitii/model/Contact.java
@@ -0,0 +1,54 @@
+package ro.petitii.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonView(DataTablesOutput.View.class)
+    private Long id;
+    
+    @JsonView(DataTablesOutput.View.class)
+    String name;
+    @JsonView(DataTablesOutput.View.class)
+    String phone;
+    
+    @NotEmpty
+    @Email
+    @JsonView(DataTablesOutput.View.class)
+    String email;
+	
+    
+    public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
---
+++ src/main/java/ro/petitii/service/ContactService.java
@@ -0,0 +1,18 @@
+package ro.petitii.service;
+
+import java.util.List;
+
+import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
+import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
+
+import ro.petitii.model.Contact;
+
+public interface ContactService {
+    Iterable<Contact> getAllContacts();
+
+    List<Contact> findUserByEmail(String email);
+    List<Contact> findUserByName(String name);
+    List<Contact> findUserByPhone(String phone);
+
+    DataTablesOutput<Contact> findAll(DataTablesInput input);
+}
