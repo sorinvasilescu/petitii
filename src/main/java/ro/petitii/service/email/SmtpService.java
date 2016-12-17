c39f35c75317030cf9b2502f1d56c8d01deccb43
Sorin Vasilescu <sorin.vasilescu@gmail.com>
2016-12-17 15:55:54 +0200

    refactored mail sending

--- src/main/java/ro/petitii/service/email/SmtpService.java
+++ src/main/java/ro/petitii/service/email/SmtpService.java
@@ -1,5 +1,7 @@
 package ro.petitii.service.email;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ro.petitii.config.SmtpConfig;
@@ -17,22 +19,17 @@ import java.util.Properties;
 public class SmtpService {
 
     @Autowired
-    SmtpConfig config;
+    private SmtpConfig config;
+
+    private Session session = null;
+    private Transport transport = null;
+    private Properties props = null;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ImapService.class);
+
 
     public void send(Email email) throws MessagingException {
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", config.getSsl());
-        props.put("mail.smtp.host", config.getServer());
-        props.put("mail.smtp.port", config.getPort());
-
-        Session session = Session.getInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(config.getUsername(), config.getPassword());
-            }
-        });
-
+        LOGGER.info("Starting mail send");
+        if ((transport == null)||(!transport.isConnected())) connect();
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress(config.getUsername()));
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email.getRecipients()));
@@ -48,6 +45,29 @@ public class SmtpService {
                 content.addBodyPart(att.getBodyPart());
             }
         message.setContent(content);
-        Transport.send(message);
+        transport.sendMessage(message,message.getAllRecipients());
+        LOGGER.info("Mail sent succesfuly");
+    }
+
+    private void connect() throws MessagingException {
+        if (props == null) {
+            props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", config.getSsl());
+            props.put("mail.smtp.host", config.getServer());
+            props.put("mail.smtp.port", config.getPort());
+        }
+        if (session == null) {
+            session = Session.getInstance(props, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(config.getUsername(), config.getPassword());
+                }
+            });
+        }
+        if ((transport == null)||(!transport.isConnected())) {
+            transport = session.getTransport("smtp");
+            transport.connect();
+        }
     }
 }
