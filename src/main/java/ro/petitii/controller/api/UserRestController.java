0269f3a0197f3c88c9a1d08ec209c2f0eccd4d6c
Ovidiu Serban <serban.ovidiu.m@gmail.com>
2016-12-28 15:36:48 +0200

    Reset password

--- src/main/java/ro/petitii/controller/api/UserRestController.java
+++ src/main/java/ro/petitii/controller/api/UserRestController.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
 import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import ro.petitii.model.User;
 import ro.petitii.service.UserService;
 
