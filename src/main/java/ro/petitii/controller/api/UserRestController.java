a4208e0b25cb703ee455f55d5a3ec3ffc121140e
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-12-29 23:13:16 +0200

    PET-20 secured api access

--- src/main/java/ro/petitii/controller/api/UserRestController.java
+++ src/main/java/ro/petitii/controller/api/UserRestController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
 import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class UserRestController {
     private UserService userService;
 
     @JsonView(DataTablesOutput.View.class)
-//    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasAuthority('ADMIN')")
     @RequestMapping(value = "/api/users", method = {RequestMethod.GET, RequestMethod.POST})
     public DataTablesOutput<User> getUsers(@Valid DataTablesInput input) {
         return userService.findAll(input);
