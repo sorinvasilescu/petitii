97fd73e1790530daa981af60f0adc84ec22c8271
Ovidiu Serban <serban.ovidiu.m@gmail.com>
2017-01-18 16:14:34 +0000

    Login

--- src/main/java/ro/petitii/controller/MainController.java
+++ src/main/java/ro/petitii/controller/MainController.java
@@ -2,7 +2,9 @@ package ro.petitii.controller;
 
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController implements ErrorController {
