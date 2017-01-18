9b9c3715cb59d5ba928cb74fbdded204d1b27137
gsergiu <sergiu.gordea@ait.ac.at>
2017-01-18 02:12:28 +0100

    #PET-79 moved messaged to properties file and added new methods in TranslationUtils
    Change-Id: I5383b57c868ab61ca4a523fa244bb45ad69b33b2

--- src/main/java/ro/petitii/util/TranslationUtil.java
+++ src/main/java/ro/petitii/util/TranslationUtil.java
@@ -1,34 +1,56 @@
 package ro.petitii.util;
 
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
-import ro.petitii.model.Email;
+import org.springframework.web.servlet.LocaleResolver;
+
 import ro.petitii.model.EmailTemplate;
 import ro.petitii.model.PetitionStatus;
 
-import java.util.Locale;
-
 @Component
 public class TranslationUtil {
-    private static final Locale ro = new Locale("ro");
+    public static final Locale ro = new Locale("ro");
 
     private static MessageSource messageSource;
-
+    
+    private static LocaleResolver localeResolver;
+	
     @Autowired
     public void setMessageSource(MessageSource messageSource) {
         TranslationUtil.messageSource = messageSource;
     }
 
-    public static String i18n(String templateId) {
-        return messageSource.getMessage(templateId, null, ro);
+    @Autowired
+    public void setLocaleResolver(LocaleResolver localeResolver) {
+        TranslationUtil.localeResolver = localeResolver;
+    }
+    
+    public static String i18n(String templateId, HttpServletRequest request) {
+        return messageSource.getMessage(templateId, null, localeResolver.resolveLocale(request));
     }
 
+    public static String i18n(String key, String args[], HttpServletRequest request) {
+    	 return messageSource.getMessage(key, null, localeResolver.resolveLocale(request));
+    }
+    
+    public static String i18n(String key, String args[]) {
+   	 return messageSource.getMessage(key, args, ro);
+    }
+    
+    public static String i18n(String key) {
+      	 return messageSource.getMessage(key, (String[])null, ro);
+    }
+    
     public static String categoryMsg(EmailTemplate.Category category) {
         return i18n("emailTemplate.category." + category.name().toLowerCase());
     }
 
     public static String resolutionMsg(PetitionStatus.Resolution resolution) {
-        return i18n("petition.resolution." + resolution.name().toLowerCase());
+        return i18n("petition.resolution." + resolution.name().toLowerCase(), (String[])null);
     }
 }
